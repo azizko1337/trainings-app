6ce9188177042a89b6d38c53f9f4a16a9bbf3959
Antoni Załupka <antoni.zalupka123@gmail.com>
2023-05-02 12:28:05 +0200

    made a little cleanup set up theme prepared global style file

---
+++ src/styles/Theme.ts
@@ -0,0 +1,19 @@
+const Theme = {
+    colors: {
+        primary: '#0070f3',
+        secondary: '#ff0000',
+        background: '#f5f5f5',
+        text: '#333333',
+        success: '#0070f3',
+        error: '#ff0000',
+    },
+    size: {
+        S: '10px',
+        M: '16px',
+        L: '24px',
+        XL: '32px',
+        XXL: '48px',
+    }
+}
+
+export default Theme;
