0530679f6e6aa658c6cb3566621a134498f49458
Antoni Załupka <antoni.zalupka123@gmail.com>
2023-05-22 22:10:35 +0200

    Moved session password to .env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,17 +1,16 @@
 import SessionUser from "@/types/SessionUser";
 
-declare module "iron-session" { 
-    interface IronSessionData { 
-      user?: SessionUser; 
-    } 
-  } 
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: SessionUser;
+  }
+}
 
 export const ironOptions = {
-    cookieName: "myapp_cookiename",
-    password: "complex_password_at_least_32_characters_long",
-    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    },
-  };
-
+  cookieName: "trainingsapp-session",
+  password: process.env.SESSION_PASSWORD,
+  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+};
